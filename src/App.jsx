b1df2842cc208e26c03e79fe432160bdb47c97d5
Jonathan <jonathankendrick697@gmail.com>
2024-04-02 08:37:46 +0100

    deleted animation

--- src/App.jsx
+++ src/App.jsx
@@ -9,58 +9,8 @@ import TestimonialSection from './component/TestimonialSection';
 import ContactUsSection from './component/ContactUsSection';
 import NewsLetterSection from './component/NewsLetterSection';
 import FooterSection from './component/Footer';
-import { gsap } from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 function App() {
-  //ANIMATIONs
-
-  //register pluginsS
-  gsap.registerPlugin(useGSAP);
-  gsap.registerPlugin(ScrollTrigger);
-
-  useGSAP(() => {
-    /* fade up animation */
-    //get all element with the classname point
-    document.querySelectorAll('.fade-up').forEach((point) => {
-      //loop through each of them and activate animation one them
-      gsap.from(point, {
-        scrollTrigger: point,
-        y: '100',
-        opacity: '0',
-        ease: 'none',
-        duration: 0.6,
-      });
-    });
-
-    /* slide in from the left */
-    //get all element with the classname point
-    document.querySelectorAll('.slide-in-left').forEach((point) => {
-      //loop through each of them and activate animation one them
-      gsap.from(point, {
-        scrollTrigger: point,
-        x: '-400',
-        opacity: '0',
-        ease: 'none',
-        duration: 0.6,
-      });
-    });
-
-    /* slide in from the right */
-    //get all element with the classname point
-    document.querySelectorAll('.slide-in-right').forEach((point) => {
-      //loop through each of them and activate animation one them
-      gsap.from(point, {
-        scrollTrigger: point,
-        x: '400',
-        opacity: '0',
-        ease: 'none',
-        duration: 0.6,
-      });
-    });
-  }, {});
-
   return (
     <>
       <Header />
